359002080cf580094b8c25ae32fe1612552a7f5c
PavelSaganov <saganovpavel@mail.ru>
2024-06-20 17:32:05 +0300

    Added some elements to MainWindow, added new folders

--- UserInfo/WPF/MVVM/View/MainWindow.xaml.cs
+++ UserInfo/WPF/MVVM/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF
+namespace WPF.MVVM.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
